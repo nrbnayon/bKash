918cb296cdb69860068c26e8e749990efbea86e3
nrbnayon <nrbnayon@gmail.com>
2024-07-17 12:51:00 +0600

    added redux for login

--- src/Pages/Auth/Login.jsx
+++ src/Pages/Auth/Login.jsx
@@ -1,10 +1,18 @@
 import { useForm } from "react-hook-form";
 import { useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
+import { useDispatch, useSelector } from "react-redux";
 import useAxiosPublic from "../../hooks/useAxiosPublic";
+import {
+  signInStart,
+  signInSuccess,
+  signInFailure,
+} from "../../Redux/Reducers/User/userSlice";
 
 const LoginPage = () => {
   const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const { loading, error: errorMessage } = useSelector((state) => state.user);
   const axiosPublic = useAxiosPublic();
   const {
     register,
@@ -13,6 +21,7 @@ const LoginPage = () => {
   } = useForm();
 
   const onSubmit = async (data) => {
+    dispatch(signInStart());
     const value = data.identifier.trim();
     if (value.includes("@")) {
       data.email = value.toLowerCase();
@@ -27,17 +36,18 @@ const LoginPage = () => {
       const response = await axiosPublic.post("/api/auth/signin", data);
       if (response.status === 200) {
         toast.success("Login successful!");
+        dispatch(signInSuccess(response.data));
         navigate("/");
       }
     } catch (error) {
       toast.error(error.response.data.message || "Login failed");
-      console.error("Login failed", error);
+      dispatch(signInFailure(error.response.data.message || "Login failed"));
     }
   };
 
   return (
     <div className="flex justify-center items-center min-h-screen bg-gradient-to-r from-pink-500 to-red-600">
-      <div className="w-full max-w-md p-8 space-y-8 bg-white rounded-lg shadow-xl">
+      <div className="w-full max-w-xl p-8 space-y-8 bg-white rounded-lg shadow-xl">
         <h2 className="text-3xl font-bold text-center text-gray-800">
           bKash Login
         </h2>
@@ -112,9 +122,13 @@ const LoginPage = () => {
             <button
               type="submit"
               className="btn btn-primary w-full py-2 px-4 bg-pink-600 text-white rounded-lg hover:bg-pink-700 focus:outline-none focus:ring-2 focus:ring-pink-500 focus:ring-offset-2"
+              disabled={loading} // Disable button when loading
             >
-              Login
+              {loading ? "Logging in..." : "Login"}
             </button>
+            {errorMessage && (
+              <p className="text-red-500 text-sm mt-2">{errorMessage}</p>
+            )}
           </div>
         </form>
       </div>
--- src/main.jsx
+++ src/main.jsx
@@ -6,14 +6,18 @@ const queryClient = new QueryClient();
 import { RouterProvider } from "react-router-dom";
 import { HelmetProvider } from "react-helmet-async";
 import { router } from "./Routers/Routers";
+import { store } from "./Redux/store";
+import { Provider } from "react-redux";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <div className="max-w-screen-xl mx-auto">
-      <QueryClientProvider client={queryClient}>
-        <HelmetProvider>
-          <RouterProvider router={router} />
-        </HelmetProvider>
-      </QueryClientProvider>
+      <Provider store={store}>
+        <QueryClientProvider client={queryClient}>
+          <HelmetProvider>
+            <RouterProvider router={router} />
+          </HelmetProvider>
+        </QueryClientProvider>
+      </Provider>
     </div>
   </React.StrictMode>
 );
